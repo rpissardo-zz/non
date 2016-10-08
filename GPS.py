9bf89338148f25b1fa73291a9f4fd6a00e2df682
rpissardo <rpissardo@hotmail.com>
2016-10-08 10:10:07 -0300

    GPS version working...Cloud9 server

---
+++ GPS.py
@@ -0,0 +1,73 @@
+
+import math
+import os
+from gps import *
+from time import *
+import time
+import threading
+import urllib2
+print("Aguardado sistema iniciar")
+time.sleep(30) 
+gpsd = None #seting the global variable
+os.system('sudo systemctl stop gpsd.socket')
+os.system('sudo systemctl disable gpsd.socket')
+os.system('sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock') 
+os.system('clear') #clear the terminal (optional)
+ 
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+ 
+if __name__ == '__main__':
+  gpsp = GpsPoller() # create the thread
+  try:
+    gpsp.start() # start it up
+    while True:
+      #It may take a second or two to get good data
+      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+ 
+      os.system('clear')
+ 
+      print
+      print ' GPS reading'
+      print '----------------------------------------'
+      print 'latitude    ' , gpsd.fix.latitude
+      print 'longitude   ' , gpsd.fix.longitude
+      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      print 'altitude (m)' , gpsd.fix.altitude
+      print 'eps         ' , gpsd.fix.eps
+      print 'epx         ' , gpsd.fix.epx
+      print 'epv         ' , gpsd.fix.epv
+      print 'ept         ' , gpsd.fix.ept
+      print 'speed (m/s) ' , gpsd.fix.speed
+      print 'climb       ' , gpsd.fix.climb
+      print 'track       ' , gpsd.fix.track
+      print 'mode        ' , gpsd.fix.mode
+      print
+      print 'sats        ' , gpsd.satellites
+      if(float('-inf')<float(gpsd.fix.latitude)<float('inf')):
+	while True:
+	  try:
+	    url = "http://fretbus-rpissardo.c9users.io/fretbus/data/newpos/1/"+str(gpsd.fix.latitude)+"/"+str(gpsd.fix.longitude)+"/"+str(gpsd.fix.speed) 
+            response  = urllib2.urlopen(url)
+            print response
+	    break
+          except:
+	    break
+      time.sleep(1) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
+
